55579a911c6b00e3ebc9aeeb66570667ccc0ebdd
RdrStels <kandakova0002@gmail.com>
2021-10-10 14:10:34 +0500

    Сортировка слиянием

--- MergeSort/Form1.Designer.cs
+++ MergeSort/Form1.Designer.cs
@@ -31,25 +31,17 @@
         {
             this.textMainFilePath = new System.Windows.Forms.TextBox();
             this.showContentBoxMain = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.showContentBoxMain1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textMainFilePath
             // 
             this.textMainFilePath.Location = new System.Drawing.Point(95, 12);
             this.textMainFilePath.Name = "textMainFilePath";
+            this.textMainFilePath.ReadOnly = true;
             this.textMainFilePath.Size = new System.Drawing.Size(332, 20);
             this.textMainFilePath.TabIndex = 0;
             // 
@@ -58,16 +50,18 @@
             this.showContentBoxMain.Location = new System.Drawing.Point(12, 57);
             this.showContentBoxMain.Multiline = true;
             this.showContentBoxMain.Name = "showContentBoxMain";
+            this.showContentBoxMain.ReadOnly = true;
             this.showContentBoxMain.Size = new System.Drawing.Size(246, 180);
             this.showContentBoxMain.TabIndex = 1;
             // 
-            // textBox3
+            // showContentBoxMain1
             // 
-            this.textBox3.Location = new System.Drawing.Point(264, 57);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(246, 180);
-            this.textBox3.TabIndex = 2;
+            this.showContentBoxMain1.Location = new System.Drawing.Point(264, 57);
+            this.showContentBoxMain1.Multiline = true;
+            this.showContentBoxMain1.Name = "showContentBoxMain1";
+            this.showContentBoxMain1.ReadOnly = true;
+            this.showContentBoxMain1.Size = new System.Drawing.Size(246, 180);
+            this.showContentBoxMain1.TabIndex = 2;
             // 
             // button1
             // 
@@ -75,7 +69,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
+            this.button1.Text = "...";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.loadFileButton_Click);
             // 
@@ -85,116 +79,40 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(77, 14);
             this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
+            this.label1.Text = "Загрузить";
             // 
-            // button2
+            // startButton
             // 
-            this.button2.Location = new System.Drawing.Point(12, 243);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(246, 23);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(435, 243);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // label2
-            // 
-            this.label2.Location = new System.Drawing.Point(12, 279);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(100, 23);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Time:";
-            // 
-            // label3
-            // 
-            this.label3.Location = new System.Drawing.Point(12, 302);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(100, 23);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "label3";
-            // 
-            // label4
-            // 
-            this.label4.Location = new System.Drawing.Point(12, 325);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(100, 23);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "label4";
-            // 
-            // label5
-            // 
-            this.label5.Location = new System.Drawing.Point(12, 348);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(100, 23);
-            this.label5.TabIndex = 10;
-            this.label5.Text = "label5";
-            // 
-            // label6
-            // 
-            this.label6.Location = new System.Drawing.Point(158, 348);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(100, 23);
-            this.label6.TabIndex = 14;
-            this.label6.Text = "label6";
-            // 
-            // label7
-            // 
-            this.label7.Location = new System.Drawing.Point(158, 325);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(100, 23);
-            this.label7.TabIndex = 13;
-            this.label7.Text = "label7";
-            // 
-            // label8
-            // 
-            this.label8.Location = new System.Drawing.Point(158, 302);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(100, 23);
-            this.label8.TabIndex = 12;
-            this.label8.Text = "label8";
-            // 
-            // label9
-            // 
-            this.label9.Location = new System.Drawing.Point(158, 279);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(100, 23);
-            this.label9.TabIndex = 11;
-            this.label9.Text = "label9";
+            this.startButton.Enabled = false;
+            this.startButton.Location = new System.Drawing.Point(12, 243);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(495, 23);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(519, 377);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(519, 277);
+            this.Controls.Add(this.startButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.showContentBoxMain1);
             this.Controls.Add(this.showContentBoxMain);
             this.Controls.Add(this.textMainFilePath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "MergeSort";
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.Button startButton;
+
+        private System.Windows.Forms.TextBox showContentBoxMain1;
 
         private System.Windows.Forms.TextBox showContentBoxMain;
 
@@ -203,15 +121,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
 
         private System.Windows.Forms.TextBox textBox3;
 
--- MergeSort/Form1.cs
+++ MergeSort/Form1.cs
@@ -17,6 +17,7 @@ namespace MergeSort
         public OpenFileDialog opnMainFile = new OpenFileDialog();
         public string mFilePath = string.Empty;
         public string aFilePath = string.Empty;
+        public int Iterators = 0;
         public OpenFileDialog mainFileLoad = new OpenFileDialog();
         public string pattern = @"([A-Za-z])+";
 
@@ -29,24 +30,35 @@ namespace MergeSort
             mainFileLoad.RestoreDirectory = true;
         }
 
-        private void ReadToFormForShowContent(string path, bool thatFile)
+        public void ReadToFormForShowContent(string path, bool thatFile)
         {
-            showContentBoxMain.Text = string.Empty;
+            startButton.Enabled = false;
+            bool empty = true;
             using (StreamReader iRead = new StreamReader(path))
             {
                 string line;
                 while ((line = iRead.ReadLine()) != null)
                 {
+                    empty = false;
                     if (!Regex.IsMatch(line, pattern))
                     {
-                        showContentBoxMain.Text += line;
+                        if(thatFile) showContentBoxMain.Text += (line + " ");
+                        else  showContentBoxMain1.Text += (line + " ");
+                        startButton.Enabled = true;
                     }
                     else
                     {
                         MessageBox.Show("В файле присутсвуют постороние символы");
                         showContentBoxMain.Clear();
+                        showContentBoxMain1.Clear();
                         return;
                     }
+                    Iterators++;
+                }
+
+                if (empty)
+                {
+                    MessageBox.Show("Файл пуст");
                 }
             }
         }
@@ -57,6 +69,7 @@ namespace MergeSort
             {
                 textMainFilePath.Text = mainFileLoad.FileName;
                 mFilePath = mainFileLoad.FileName;
+                showContentBoxMain.Clear();
                 ReadToFormForShowContent(mFilePath, true);
             }
         }
@@ -70,7 +83,7 @@ namespace MergeSort
             //show result
         }
 
-        private void SplitMainFile(string pathMainFile)
+        public void SplitMainFile(string pathMainFile)
         {
             //Creating additional files
             FileStream fs_first = new FileStream("adF1.ms", FileMode.OpenOrCreate);
@@ -79,101 +92,118 @@ namespace MergeSort
             fs_second.Close();
             using (StreamReader mainFile = File.OpenText(pathMainFile))
             {
-                //Reading main file
-                string line = string.Empty;
-                int i = 0;
-                while (( line = mainFile.ReadLine()) != null)
+                using (StreamWriter fs1 = new StreamWriter("adF1.ms"), fs2 = new StreamWriter("adF2.ms"))
                 {
-                    using(StreamWriter fs1 = new StreamWriter("adF1.ms"), fs2 = new StreamWriter("adF2.ms"))
+                    //Reading main file
+                    string line = string.Empty;
+                    int i = 0;
+                    while (( line = mainFile.ReadLine()) != null)
                     {
-                        int temp = Convert.ToInt32(line);
-                        var t = new UTF8Encoding(true).GetBytes(Convert.ToString(temp));
+                    
+                        var t = new UTF8Encoding(true).GetBytes(Convert.ToString(line));
                         if (i % 2 == 0)
-                            fs1.WriteLine(t);
+                            fs1.WriteLine(line);
                         else
-                            fs2.WriteLine(t);
+                            fs2.WriteLine(line);
+                        i++;
                     }
+                    
                 }
             }
-             
         }
-
-        private void SimpleMergeSort(string pathMainFile)
+        public bool isSorter(string pathMainFile)
         {
-            int k = 1, a1 = 0, a2 = 0;
-            int f1 = 0, f2 = 0;
-            using (StreamReader af1 = File.OpenText("adF1.ms"), af2 = File.OpenText("adF2.ms"))
+            using (StreamReader af1 = File.OpenText(pathMainFile))
             {
-                string lineOfFile1 = string.Empty, lineOfFile2 = string.Empty;
-                while ((lineOfFile1 = af1.ReadLine()) != null && (lineOfFile2 = af2.ReadLine()) != null)
+                string a1 = "0";
+                string a2 = "0";
+                a1 = af1.ReadLine();
+                a2 = af1.ReadLine();
+                while (a1 != null && a2  != null)
                 {
-                    int i = 0, j = 0;
-                    f1 = 0; f2 = 0;
-                    while (i < k && j < k && ((lineOfFile1 = af1.ReadLine()) != null) &&((lineOfFile2 = af2.ReadLine()) != null))
+                    if (Convert.ToInt32(a1) > Convert.ToInt32(a2))
+                        return false;
+                    else
                     {
-                        a1 = Convert.ToInt32(lineOfFile1);
-                        a2 = Convert.ToInt32(lineOfFile2);
-                        using (FileStream fs = new FileStream("temp.txt",FileMode.OpenOrCreate))
+                        a1 = a2;
+                        a2 = af1.ReadLine();
+                    }
+
+                }        
+            }
+            return true;
+        }
+        public void SimpleMergeSort(string pathMainFile)
+        {
+            int k = 1;
+            string a1;
+            string a2;
+            bool b;
+            int f1 = 0, f2 = 0;
+            while (true) 
+            {
+                if(isSorter(pathMainFile)) 
+                    break;
+                SplitMainFile(pathMainFile);
+                using (StreamReader af1 = File.OpenText("adF1.ms"), af2 = File.OpenText("adF2.ms"))
+                {
+                    using (StreamWriter fs = new StreamWriter(pathMainFile))
+                    {
+                        string lineOfFile1 = string.Empty, lineOfFile2 = string.Empty; 
+                        a1 = af1.ReadLine();
+                        a2 = af2.ReadLine();
+                        b = true;
+                        while (a2 != null && a1 != null)
                         {
-                            if (a1 < a2)
+                            int i = 0, j = 0;
+                            while ((i < k || j < k) && a2 != null && a1 != null)
                             {
-                                var t =  new UTF8Encoding(true).GetBytes(Convert.ToString(a1));
-                                fs.Write(t, 0 , t.Length);
-                                a1 = Convert.ToInt32(lineOfFile1);
+                                if (Convert.ToInt32(a1) < Convert.ToInt32(a2))
+                                {
+                                    fs.WriteLine(a1);
+                                    a1 = af1.ReadLine();
+                                    i++;
+                                }
+                                else
+                                {
+                                    fs.WriteLine(a2);
+                                    a2 = af2.ReadLine();
+                                    j++;
+                                }
+                            }
+                            while (i < k && a1 != null)
+                            {
+                                fs.WriteLine(a1);
+                                a1 = af1.ReadLine();
                                 i++;
                             }
-                            else
+                            while (j < k && a2 != null)
                             {
-                                var t =  new UTF8Encoding(true).GetBytes(Convert.ToString(a2));
-                                fs.Write(t, 0 , t.Length);
-                                a2 = Convert.ToInt32(lineOfFile2);
+                                fs.WriteLine(a2);
+                                a2 = af2.ReadLine();
                                 j++;
-                            }
+                            } 
                         }
+                        while (a1 != null)
+                        {
+                            fs.WriteLine(a1);
+                            a1 = af1.ReadLine();
+                        }
+                        while (a2 != null)
+                        {
+                            fs.WriteLine(a2);
+                            a2 = af2.ReadLine();
+                        } 
+                        k*=2;
                     }
-                    using (FileStream fs = new FileStream("temp.txt", FileMode.OpenOrCreate))
-                    {
-                        while (i<k && (lineOfFile1 = af1.ReadLine()) != null)
-                        {
-                            var t =  new UTF8Encoding(true).GetBytes(Convert.ToString(a1));
-                            fs.Write(t, 0 , t.Length);
-                            a1 = Convert.ToInt32(lineOfFile1);
-                            i++;
-                        }
-                        while (j<k && (lineOfFile2 = af2.ReadLine()) != null)
-                        {
-                            var t =  new UTF8Encoding(true).GetBytes(Convert.ToString(a2));
-                            fs.Write(t, 0 , t.Length);
-                            a2 = Convert.ToInt32(lineOfFile2);
-                            j++;
-                        }
-                    }
-                    using (FileStream fs = new FileStream("temp.txt", FileMode.OpenOrCreate))
-                    {
-                        while ((lineOfFile1 = af1.ReadLine()) != null)
-                        {
-                            var t =  new UTF8Encoding(true).GetBytes(Convert.ToString(a1));
-                            fs.Write(t, 0 , t.Length);
-                            a1 = Convert.ToInt32(lineOfFile1);
-                            i++;
-                        }
-                        while ((lineOfFile2 = af2.ReadLine()) != null)
-                        {
-                            var t =  new UTF8Encoding(true).GetBytes(Convert.ToString(a2));
-                            fs.Write(t, 0 , t.Length);
-                            a2 = Convert.ToInt32(lineOfFile2);
-                            j++;
-                        }
-                    }
-                    k *= 2;
                 }
             }
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
-            SplitMainFile(mFilePath);
+            showContentBoxMain1.Clear();
             SimpleMergeSort(mFilePath);
+            ReadToFormForShowContent(mFilePath, false);
         }
     }
 }
